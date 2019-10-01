33b56a651bea5e51d1e825a60cb915a13df01167
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 19:24:09 +0300

    Update Program.cs

--- Lab1_6/Program.cs
+++ Lab1_6/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +11,9 @@ namespace Lab1_6
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number: ");
-            string snumber = Console.ReadLine();
-            int number = int.Parse(snumber);
-            if ((int)(number / 1000)<10)
+            int number = int.Parse(Console.ReadLine());
+            if (((int)(number / 1000) < 10) && ((int)(number / 1000) > 0))
             {
-                if ((int)(number / 1000) > 0)
-                {
                     int first = (int)(number / 1000);
                     int second = (int)(number % 1000 / 100);
                     int third = (int)(number % 100 / 10);
@@ -24,14 +21,10 @@ namespace Lab1_6
                     int product = first * second * third * fourth;
                     Console.WriteLine("The product is " + product);
                     Console.ReadKey();
-                }
-                else
-                    Console.WriteLine("Error");
-                    Console.ReadKey();
             }
             else
                 Console.WriteLine("Error");
-                Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
