478c33f8aaad04cbff864f01228ff6d4d01feafe
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 19:41:08 +0300

    Update Program.cs

--- Lab1_1/Program.cs
+++ Lab1_1/Program.cs
@@ -10,9 +10,10 @@ namespace Lab1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number:");
+            Console.Write("Enter the number:");
             decimal number = decimal.Parse(Console.ReadLine());
             int result = (int)((number - (int)(number))*10);
+            Console.WriteLine();
             Console.WriteLine("The first digit of the fractional part: " + result);
         }
     }
