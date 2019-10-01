8dd88f10d929ea666c1fc6ee3ad16c830f384cdb
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 19:26:19 +0300

    Update Program.cs

--- Lab1_7/Program.cs
+++ Lab1_7/Program.cs
@@ -11,21 +11,14 @@ namespace Lab1_7
         static void Main(string[] args)
         {
             Console.WriteLine("Enther the number: ");
-            string snumber = Console.ReadLine();
-            int number = int.Parse(snumber);
-            if ((int)(number / 100) < 10)
+            int number = int.Parse(Console.ReadLine());
+            if (((int)(number / 100) < 10) && ((int)(number / 100) > 0))
             {
-                if ((int)(number / 100) > 0)
-                {
                     int first = number / 100;
                     int second = (int)(number % 100 / 10);
                     int third = number % 10;
                     int reserved = third * 100 + second * 10 + first;
                     Console.WriteLine("Reserved number: " + reserved);
-                }
-                else
-                    Console.WriteLine("Error");
-                    Console.ReadKey();
             }
             else
                 Console.WriteLine("Error");
