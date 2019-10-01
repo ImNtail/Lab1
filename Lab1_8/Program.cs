9e7afd2bf1a53d9b993f7c62852c19f139cbd36a
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 19:26:59 +0300

    Update Program.cs

--- Lab1_8/Program.cs
+++ Lab1_8/Program.cs
@@ -11,8 +11,7 @@ namespace Lab1_8
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the X for an expression 3x^4-5x^3+2x^2-x+7: ");
-            string sx = Console.ReadLine();
-            double x = double.Parse(sx);
+            double x = double.Parse(Console.ReadLine());
             double @expression = x * (x * (x * (3 * x - 5) + 2) - 1) + 7;
             Console.WriteLine("The expression is equal: " + @expression);
             Console.ReadKey();
