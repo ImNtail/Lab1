da2792fa354a2211cd3218daabf1f7eab64ecde0
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 18:08:11 +0300

    Lab1_4

---
+++ Lab1_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            string sx = Console.ReadLine();
+            int x = int.Parse(sx);
+            Console.WriteLine("Enter the second number: ");
+            string sy = Console.ReadLine();
+            int y = int.Parse(sy);
+            x = x + y;
+            y = x - y;
+            x = x - y;
+            Console.WriteLine("The first number is " + x);
+            Console.WriteLine("The second number is " + y);
+            Console.ReadKey();
+        }
+    }
+}
