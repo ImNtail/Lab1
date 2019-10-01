4454324a7c7fa49f607c04e0e72256ab5e42760a
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 18:58:44 +0300

    Lab1_8

---
+++ Lab1_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the X for an expression 3x^4-5x^3+2x^2-x+7: ");
+            string sx = Console.ReadLine();
+            double x = double.Parse(sx);
+            double @expression = x * (x * (x * (3 * x - 5) + 2) - 1) + 7;
+            Console.WriteLine("The expression is equal: " + @expression);
+            Console.ReadKey();
+        }
+    }
+}
