030282098a6deeca4f53c98610733d7276857ff6
Ntail <55882052+ImNtail@users.noreply.github.com>
2019-10-01 18:06:07 +0300

    Lab1_2

---
+++ Lab1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many seconds are gone?");
+            string sec_text = Console.ReadLine();
+            int seconds = int.Parse(sec_text);
+            int hours = (int)(seconds / 3600);
+            int minutes = (int)(seconds / 60);
+            while (minutes > 60) 
+            {
+                minutes = minutes - 60;
+            }
+            Console.WriteLine("Time are gone: " + hours + " hours " + minutes + " minutes");
+        }
+    }
+}
